124574a5f5c745c865f0222d69bc95cd0b3e50e9
NikitaS000 <132354596+NikitaS000@users.noreply.github.com>
2024-06-03 19:17:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,107 +1,71 @@
 $(document).ready(function() {
     let isDragging = false;
-    const comparisonWidth = 370;
-    const totalWidth = comparisonWidth + 1086;
-
-    $('.slider').mousedown(function() {
-        isDragging = true;
-    });
-
-    $(document).mouseup(function() {
-        isDragging = false;
-    });
-
-    $(document).mousemove(function(e) {
-        if (isDragging) {
-            const containerOffset = $('.comparison-container').offset().left;
-            const mouseX = e.pageX - containerOffset;
-            if (mouseX >= 0 && mouseX <= totalWidth) {
-                $('.slider').css('left', mouseX + 'px');
-                if (mouseX > comparisonWidth) {
-                    const textContainerOffset = mouseX - comparisonWidth;
-                    const textPercentage = (textContainerOffset / 1086) * 100;
-                    $('.text-image').eq(0).css('clip-path', 'inset(0 0 0 ' + textContainerOffset + 'px)');
-                    $('.text-image').eq(1).css('clip-path', 'inset(0 ' + (1086 - textContainerOffset) + 'px 0 0)');
-                }
-            }
-        }
-    });
-});
-
-
-$(document).ready(function() {
-    let isDragging = false;
-    const comparisonWidth = 370;
-    const totalWidth = comparisonWidth + 1086;
-
-    $('.slider').mousedown(function() {
-        isDragging = true;
-    });
-
-    $(document).mouseup(function() {
-        isDragging = false;
-    });
-
-    $(document).mousemove(function(e) {
-        if (isDragging) {
-            const containerOffset = $('.comparison-container').offset().left;
-            const mouseX = e.pageX - containerOffset;
-            if (mouseX >= 0 && mouseX <= totalWidth) {
-                $('.slider').css('left', mouseX + 'px');
-                if (mouseX <= comparisonWidth) {
-                    $('.new-window').css('clip', 'rect(0, ' + mouseX + 'px, 180px, 0)');
-                }
-            }
-        }
-    });
-});
-// 
-
-
-$(document).ready(function() {
-    let isDragging = false;
     let isDelaying = false;
-    const comparisonWidth = 370;
-    const totalWidth = comparisonWidth + 1086;
-    const buffer = 50; 
-    const images = ['banner_images/beautiful.png', 'banner_images/convinient.png', 'banner_images/reliable.png', 'banner_images/silent.png', 'banner_images/warm.png'];
-    let firstIndex = 1; 
+    const comparisonWidth = $('.comparison').width();
+    const buffer = 50;
+    const images = ['banner_images/beautiful.svg', 'banner_images/convinient.svg', 'banner_images/reliable.svg', 'banner_images/silent.svg', 'banner_images/warm.svg'];
+    let firstIndex = 1;
     let secondIndex = 0;
-
 
     function updateTextImages() {
         $('.text-image').eq(0).attr('src', images[firstIndex]);
         $('.text-image').eq(1).attr('src', images[secondIndex]);
     }
 
-    $('.slider').mousedown(function() {
+    function updateClipPath(mouseX) {
+        const textContainerOffset = mouseX - comparisonWidth;
+        const textContainerWidth = $('.text-container').width();
+        if (mouseX > comparisonWidth && mouseX <= comparisonWidth + textContainerWidth) {
+            const textPercentage = (textContainerOffset / textContainerWidth) * 100;
+            $('.text-image').eq(0).css('clip-path', 'inset(0 0 0 ' + textPercentage + '%)');
+            $('.text-image').eq(1).css('clip-path', 'inset(0 ' + (100 - textPercentage) + '% 0 0)');
+        }
+    }
+
+    $('.slider').on('mousedown touchstart', function(e) {
         isDragging = true;
+        e.preventDefault();
     });
 
-    $(document).mouseup(function() {
+    $(document).on('mouseup touchend', function() {
         isDragging = false;
-        isDelaying = false; 
+        isDelaying = false;
     });
 
-    $(document).mousemove(function(e) {
-        if (isDragging && !isDelaying) {
+    $(document).on('mousemove touchmove', function(e) {
+        if (isDragging) {
             const containerOffset = $('.comparison-container').offset().left;
-            const mouseX = e.pageX - containerOffset;
+            let mouseX;
+            if (e.type === 'touchmove') {
+                mouseX = e.originalEvent.touches[0].pageX - containerOffset;
+            } else {
+                mouseX = e.pageX - containerOffset;
+            }
+
+            const totalWidth = comparisonWidth + $('.text-container').width();
             if (mouseX >= 0 && mouseX <= totalWidth) {
                 $('.slider').css('left', mouseX + 'px');
-                if (mouseX > totalWidth - buffer) { 
-                    firstIndex = (firstIndex + 1) % images.length; 
+                if (mouseX <= comparisonWidth) {
+                    $('.new-window').css('clip', 'rect(0, ' + mouseX + 'px, 180px, 0)');
+                } else {
+                    updateClipPath(mouseX);
+                }
+            }
+
+            if (mouseX >= 0 && mouseX <= totalWidth && !isDelaying) {
+                if (mouseX > totalWidth - buffer) {
+                    firstIndex = (firstIndex + 1) % images.length;
                     updateTextImages();
-                    isDelaying = true; 
+                    isDelaying = true;
                     setTimeout(function() {
-                        isDelaying = false; 
+                        isDelaying = false;
                     }, 300);
-                } else if (mouseX < buffer) { 
-                    secondIndex = (secondIndex + 1) % images.length; 
+                } else if (mouseX < buffer) {
+                    secondIndex = (secondIndex + 1) % images.length;
                     updateTextImages();
-                    isDelaying = true; 
+                    isDelaying = true;
                     setTimeout(function() {
-                        isDelaying = false; 
+                        isDelaying = false;
                     }, 300);
                 }
             }
